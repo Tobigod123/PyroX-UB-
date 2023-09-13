32f9b084bbb7e1d1551d3896553a90324b90e005
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-09-13 08:14:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ GROUP_ID = os.getenv("GROUP_ID")
 SOURCE = "https://github.com/TeamX-Coders/PyroX-UB"
 LOG_CHAT = os.getenv("LOG_CHAT")
 BOTUSERNAME = os.getenv("BOTUSERNAME")
+OWNER_ID = [1130243906, 2020270268]
